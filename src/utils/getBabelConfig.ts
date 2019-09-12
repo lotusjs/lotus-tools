3804b1d1bfb20f9c1c624d5f508c504bf87215a0
wangxingkang <wangxingkang@sensoro.com>
2019-09-12 23:20:43 +0800

    feat: 支持自定义babel配置，现只支持插件

--- src/utils/getBabelConfig.ts
+++ src/utils/getBabelConfig.ts
@@ -1,36 +1,26 @@
 import { resolve } from './utils';
+import configLoader from './config-loader';
+
+/**
+ * 获取用户Babel配置
+ * @param cwd 用户的执行目录
+ */
+const getUserBabelConfig = (cwd?: string) => {
+  cwd = cwd || process.cwd();
+
+  const babelConfig = configLoader.loadSync({
+    files: [
+      'babel.config.js',
+      'babel.config.ts'
+    ],
+    cwd
+  });
+
+  return babelConfig.data || {};
+};
 
 export default function (modules: boolean) {
-  const plugins = [
-    [
-      resolve('@babel/plugin-transform-typescript'),
-      {
-        isTSX: true,
-      }
-    ],
-    resolve('babel-plugin-inline-import-data-uri'),
-    resolve('@babel/plugin-transform-member-expression-literals'),
-    resolve('@babel/plugin-transform-object-assign'),
-    resolve('@babel/plugin-transform-property-literals'),
-    [
-      resolve('@babel/plugin-transform-runtime'),
-      {
-        helpers: false
-      }
-    ],
-    resolve('@babel/plugin-transform-spread'),
-    resolve('@babel/plugin-transform-template-literals'),
-    resolve('@babel/plugin-proposal-export-default-from'),
-    resolve('@babel/plugin-proposal-export-namespace-from'),
-    resolve('@babel/plugin-proposal-object-rest-spread'),
-    [
-      resolve('@babel/plugin-proposal-decorators'),
-      {
-        legacy: true
-      }
-    ],
-    resolve('@babel/plugin-proposal-class-properties')
-  ];
+  const { plugins = [] } = getUserBabelConfig();
 
   return {
     presets: [
@@ -52,6 +42,43 @@ export default function (modules: boolean) {
         }
       ]
     ],
-    plugins
+    plugins: [
+      [
+        resolve('@babel/plugin-transform-typescript'),
+        {
+          isTSX: true,
+        }
+      ],
+      resolve('babel-plugin-inline-import-data-uri'),
+      resolve('@babel/plugin-transform-member-expression-literals'),
+      resolve('@babel/plugin-transform-object-assign'),
+      resolve('@babel/plugin-transform-property-literals'),
+      [
+        resolve('@babel/plugin-transform-runtime'),
+        {
+          helpers: false
+        }
+      ],
+      resolve('@babel/plugin-transform-spread'),
+      resolve('@babel/plugin-transform-template-literals'),
+      resolve('@babel/plugin-proposal-export-default-from'),
+      resolve('@babel/plugin-proposal-export-namespace-from'),
+      resolve('@babel/plugin-proposal-object-rest-spread'),
+      [
+        resolve('@babel/plugin-proposal-decorators'),
+        {
+          legacy: true
+        }
+      ],
+      resolve('@babel/plugin-proposal-class-properties'),
+      [
+        resolve('babel-plugin-import'),
+        {
+          libraryName: 'antd',
+          style: true
+        }
+      ],
+      ...plugins
+    ]
   };
 }
